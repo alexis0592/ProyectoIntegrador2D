8670882a01b3d60c913cfe9745124b91a16da3cb
Yefry Calderón <yefry.calderon@udea.edu.co>
2015-05-07 22:28:56 -0500

    first commit
    first commit for Unity 2D project UdeA

---
+++ 2DTest/Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	Vector3 target;
+	float timer;
+	int sec;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		target = ResetTarget ();
+		sec = ResetSec ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (timer > sec) {
+			target = ResetTarget ();
+			sec = ResetSec ();
+		}
+		transform.Translate (target * 5 * Time.deltaTime);
+
+		if(transform.position.x <= -12.0f || transform.position.x >= 12.0f ||
+		    transform.position.y <= -3.0f || transform.position.y >= 3.0f){
+
+			//target = ResetTarget();
+			Vector2 initialPosition = new Vector2(0, 0);
+			target = initialPosition;
+			//transform.Translate(target *5* Time.deltaTime);
+		}
+		 
+	}
+	
+	private Vector3 ResetTarget ()
+	{
+		Vector3 v3 = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range (-2.0f, 2.0f));
+		return v3;
+	}
+	
+	private int ResetSec ()
+	{
+		timer = 0;
+		return Random.Range(1, 3);
+	}
+}
---
+++ 2DTest/Assets/Scripts/Rotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	private Quaternion rotTarget;
+	private float rotateEverySecond = 1;
+	private float lerpCounter;
+	private Quaternion rotCache;
+	
+	// Use this for initialization
+	void Start () {
+		randomRot ();
+		InvokeRepeating("randomRot", 0,rotateEverySecond);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Lerp(transform.rotation, rotTarget, lerpCounter*Time.deltaTime);
+		lerpCounter++;
+	}
+	
+	private void randomRot() {
+		rotTarget = Random.rotation;
+		rotCache = transform.rotation;
+		lerpCounter = 0;
+	}
+}
