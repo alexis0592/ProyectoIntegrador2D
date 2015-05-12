b85445b32f4254a44a00abb1f3bf07a8b9ca20fb
Cristian Correa <ccamilo.correag@gmail.com>
2015-05-11 20:35:37 -0500

    Ecuaciones de pitch and roll para la mira.

--- 2DTest/Assets/Scripts/acelerometer.cs
+++ 2DTest/Assets/Scripts/acelerometer.cs
@@ -10,7 +10,20 @@ public class acelerometer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Input.acceleration.x * 0.3f, Input.acceleration.y * 0.3f,0);
-		transform.eulerAngles = new Vector3(pitch,0,roll)
+		//transform.Translate(Input.acceleration.x * 0.3f, Input.acceleration.y * 0.3f,0);
+		transform.eulerAngles = new Vector3 (pitch (), 0, roll());
+	}
+
+	private float pitch(){
+		float Gx = Input.acceleration.x;
+		float Gy = Input.acceleration.y;
+		float Gz = Input.acceleration.z;
+		return Mathf.Atan(Gy/(Mathf.Sqrt(Mathf.Pow(Gx,2) + Mathf.Pow(Gz,2))));
+	}
+
+	private float roll(){
+		float Gx = Input.acceleration.x;
+		float Gz = Input.acceleration.z;
+		return Mathf.Atan(-Gx/Gz);
 	}
 }
