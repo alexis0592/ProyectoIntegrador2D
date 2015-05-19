eef2c3a2190d89490f732ea68c30aac5e0eb4049
Cristian Correa <ccamilo.correag@gmail.com>
2015-05-19 11:19:35 -0500

    Corrección de errores. Ajuste de fórmulas en el pitch and roll

--- 2DTest/Assets/Scripts/acelerometer.cs
+++ 2DTest/Assets/Scripts/acelerometer.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class acelerometer : MonoBehaviour {
 
+	float Gx = 0f;
+	float Gy = 0f;
+	float Gz = 0f;
+	static float alpha = 0.8f;
+	Vector3 vector;
+
+
+	//float yRotation = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,20 +19,27 @@ public class acelerometer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Gx = Input.acceleration.x * alpha + (Gx * (1.0f - alpha));
+		Gy = Input.acceleration.y * alpha + (Gy * (1.0f - alpha));
+		Gz = Input.acceleration.z * alpha + (Gz * (1.0f - alpha));
+
+
 		//transform.Translate(Input.acceleration.x * 0.3f, Input.acceleration.y * 0.3f,0);
-		transform.eulerAngles = new Vector3 (pitch (), 0, roll());
+		//yRotation += Input.GetAxis("Horizontal");
+		//transform.eulerAngles = new Vector3(10, yRotation, 0);
+
+		vector = new Vector3 (roll(), - pitch (), 0);
+		transform.eulerAngles = vector;
+		transform.Translate(vector);
+
+
 	}
 
 	private float pitch(){
-		float Gx = Input.acceleration.x;
-		float Gy = Input.acceleration.y;
-		float Gz = Input.acceleration.z;
 		return Mathf.Atan(Gy/(Mathf.Sqrt(Mathf.Pow(Gx,2) + Mathf.Pow(Gz,2))));
 	}
 
 	private float roll(){
-		float Gx = Input.acceleration.x;
-		float Gz = Input.acceleration.z;
 		return Mathf.Atan(-Gx/Gz);
 	}
 }
